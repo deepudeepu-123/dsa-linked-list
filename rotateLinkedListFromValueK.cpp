5709945e38dcf38d10044c9a74d504eb50664c59
unknown <dpakkumar987@gmail.com>
2023-06-06 20:19:35 +0530

    first commit

--- rotateLinkedListFromValueK.cpp
+++ rotateLinkedListFromValueK.cpp
@@ -11,52 +11,50 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-                if (head == NULL || head->next == NULL) {
+//                 if (head == NULL || head->next == NULL) {
+//             return head;
+//         }
+//         ListNode* dummy = head;
+//         int len = 1;
+//         while (dummy->next != NULL) {
+//             len++;
+//             dummy = dummy->next;
+//         }
+//         dummy->next = head;
+//         k = k % len;
+//         int end = len - k;
+//         dummy = head;
+//         while (end > 1) {
+//             end--;
+//             dummy = dummy->next;
+//         }
+//         head = dummy->next;
+//         dummy->next = NULL;
+
+//         return head;
+
+         if(k==0||head==NULL||head->next==NULL) 
             return head;
+        int n=1;
+        ListNode *t1=head,*t2,*t3;
+        while(t1->next!=NULL) 
+        {
+          n++;
+            t1=t1->next;
         }
-        ListNode* dummy = head;
-        int len = 1;
-        while (dummy->next != NULL) {
-            len++;
-            dummy = dummy->next;
+        t1->next=head;
+        k=k%n;
+        t1=head;
+        for(int i=0;i<n-k-1;i++) 
+        {
+         t1=t1->next; 
         }
-        dummy->next = head;
-        k = k % len;
-        int end = len - k;
-        dummy = head;
-        while (end > 1) {
-            end--;
-            dummy = dummy->next;
-        }
-        head = dummy->next;
-        dummy->next = NULL;
-
+        head=t1->next;
+        t1->next=NULL;
+        t3=t2;
+        
+    
         return head;
-
-        //  if(k==0||head==NULL||head->next==NULL) 
-        //     return head;
-        // int n=0;
-        // ListNode *t1=head,*t2,*t3;
-        // while(t1!=NULL) 
-        // {
-        //   n++;
-        //     t1=t1->next;
-        // }
-        // k=k%n;
-        // t1=head;
-        // for(int i=0;i<k;i++) 
-        // {
-        //  t1=t1->next; 
-        // }
-        // t2=t1->next;
-        // t1->next=NULL;
-        // t3=t2;
-        // while(t3!=NULL && t3->next!=NULL) 
-        //     {
-        //     t3=t3->next;
-        // }
-        // t3->next=head;
-        // return t2;
     //    if(head == NULL||head->next == NULL) return head;
     // for(int i=0;i<k;i++) {
     //     ListNode* temp = head;
